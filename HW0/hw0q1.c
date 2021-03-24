608d91797477470f768f0f6c7a7e21549dddac13
Danny <daniel.belokon@gmail.com>
2021-03-24 19:27:20 +0200

    hw0q1 init (and final)

---
+++ HW0/hw0q1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    // Counting from 3
+    for (int i = 3; i > 0; i--)
+    {
+        printf("%d\n", i);
+    }
+    printf("Are you ready to rumble? Ohh yeah!\n");
+    /*
+    Well, 
+    Are you ready?
+    */
+    return 0;
+}
