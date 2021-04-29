e4d320c1e1893c9a79e9fe7137acec4fd4214cfa
Danny <daniel.belokon@gmail.com>
2021-04-29 10:36:21 +0300

    implement hw2q1 - ascii info displayer

--- HW2/hw2q1.c
+++ HW2/hw2q1.c
@@ -11,28 +11,68 @@
   Constants and definitions:
 ==========================================================================*/
 
-/* put your #defines and typedefs here*/
+#define SPACE_INDEX 0
+#define ALPHA_INDEX 1
+#define NMRCAL_INDEX 2
+
+#define CHAR_TYPES_AMNT 3
 
 void printGivenParamsToTheOutput(
     char ch, int asciiCode, int asciiCodePowTwo,
     int difference, int unitDigitOfDiff
 );
 
+void determinCharType(char ch, int chTypeCntArr[CHAR_TYPES_AMNT]);
+
 void printResults(int digitsCounter, int lettersCounter, int spacesCounter);
 
 /*-------------------------------------------------------------------------
-  The main program. (describe what your program does here)
+  Print character ascii information
+  Upon receiving a stream of characters, iterate over it and print the ascii
+  representation, it's square, differaence to the previous char, and the
+  first digit of the differance. 
+  Upon completion print the summation of specific character types recieved
  -------------------------------------------------------------------------*/
 int main()
 {
+    unsigned char ch = 0;                    // current char read
+    int prev = 0;                            // previous ch
+    int chTypeCntArr[CHAR_TYPES_AMNT] = {0}; // char type counter array by index
+
+    //
+    while (scanf("%c", &ch) == 1 && ch != ';')
+    {
+        int difference = ch - prev;
+        printGivenParamsToTheOutput(ch, (int)ch, ch * (int)ch,
+                                    difference, difference % 10);
+
+        determinCharType(ch, chTypeCntArr);
+        prev = ch;
+    }
+
+    printResults(chTypeCntArr[NMRCAL_INDEX],
+                 chTypeCntArr[ALPHA_INDEX],
+                 chTypeCntArr[SPACE_INDEX]);
 
     return 0;
 }
 
+/*
+    Update the indexed counters of the passed array by using the int variable
+    Counter indexes are derived from TYPE_INDEX preproccesor definitions
+*/
+void determinCharType(char ch, int chTypeCntArr[CHAR_TYPES_AMNT])
+{
+    chTypeCntArr[SPACE_INDEX] += ch == ' ';
+    chTypeCntArr[ALPHA_INDEX] += ((ch <= 'Z' && ch >= 'A') ||
+                                  (ch <= 'z' && ch >= 'a'));
+    chTypeCntArr[NMRCAL_INDEX] += (ch <= '9' && ch >= '0');
+}
+
 void printGivenParamsToTheOutput(
     char ch, int asciiCode, int asciiCodePowTwo,
-    int difference, int unitDigitOfDiff
-){
+    int difference, int unitDigitOfDiff)
+{
     printf(
         "%c%10d%10d%10d%10d\n", ch, asciiCode,
         asciiCodePowTwo, difference, unitDigitOfDiff
