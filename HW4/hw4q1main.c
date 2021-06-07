9c3aaf380d29021b6c30e819b44b357d814e7ca8
Danny <daniel.belokon@gmail.com>
2021-06-07 18:31:04 +0300

    implement sum and average hw4q1

--- HW4/hw4q1main.c
+++ HW4/hw4q1main.c
@@ -50,6 +50,55 @@ int main() {
     return 0;
 }
 
+void compute_integral_image(int image[][M], int n, int m, int integral_image[][M])
+{
+
+    int above, left, remove;
+    for (int row = 0; row < n; row++)
+    {
+        for (int col = 0; col < m; col++)
+        {
+            above = left = remove = 0;
+            if (row > 0)
+                above = integral_image[row - 1][col];
+            if (col > 0)
+                left = integral_image[row][col - 1];
+            if (col == 0 && row == 0)
+                remove = integral_image[row - 1][col - 1];
+
+            integral_image[row][col] = image[row][col] + above + left - remove;
+        }
+    }
+}
+int sum_rect(int integral_image[][M], int rect[4])
+{
+    int sum = 0;
+    sum += integral_image[rect[2]][rect[3]];
+
+    if (rect[0] > 0)
+        sum += integral_image[rect[0] - 1][rect[3]];
+
+    if (rect[1] > 0)
+        sum += integral_image[rect[2]][rect[1] - 1];
+
+    if (rect[1] > 0 && rect[0] > 0)
+        sum -= integral_image[rect[0] - 1][rect[1] - 1];
+
+    return sum;
+}
+
+void sliding_average(int integral_image[][M], int n, int m, int h, int w, int average[][M])
+{
+    int rect[4];
+    for (int row = 0; row < n; row++)
+    {
+        for (int col = 0; col < m; col++)
+        {
+            /* code */
+        }
+    }
+}
+
 void print_array(int a[N][M], int n, int m) {
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < m; ++j) {
