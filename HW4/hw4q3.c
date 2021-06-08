f96a15b61589880eac13ea16d741a82e460e7b0e
Danny <daniel.belokon@gmail.com>
2021-06-08 18:58:48 +0300

    hw4q3

--- HW4/hw4q3.c
+++ HW4/hw4q3.c
@@ -8,22 +8,22 @@ int main()
     switch (question)
     {
     case 1 :
-        printf("The answer to question 1 is a\n");
+        printf("The answer to question 1 is b\n");
         break;
     case 2 :
-        printf("The answer to question 2 is a\n");
+        printf("The answer to question 2 is r\n");
         break;
     case 3 :
-        printf("The answer to question 3 is a\n");
+        printf("The answer to question 3 is p\n");
         break;
     case 4 :
         printf("The answer to question 4 is a\n");
         break;
     case 5 :
-        printf("The answer to question 5 is a\n");
+        printf("The answer to question 5 is l\n");
         break;
     case 6 :
-        printf("The answer to question 6 is a\n");
+        printf("The answer to question 6 is g\n");
         break;
     default :
         printf("Error!\n");
