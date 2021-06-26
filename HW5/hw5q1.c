c9b14b9d39283829c1be69fedb3d5559b3f58b94
Danny <daniel.belokon@gmail.com>
2021-06-26 10:21:53 +0300

    hw5q1 initial

---
+++ HW5/hw5q1.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int find_largest(int nums_left, int cur_largest, int second_largest);
+
+int main()
+{
+    int iterations = 0;
+    int second_largest = 0;
+    printf("Please enter length:\n");
+    if (scanf("%d", &iterations) > 0)
+    {
+        printf("Please enter array:\n");
+        second_largest = find_largest(iterations, 0, 0);
+        printf("The second-greatest is:\n%d", second_largest);
+    }
+}
+
+int find_largest(int nums_left, int cur_largest, int second_largest)
+{
+    if (nums_left == 0)
+        return second_largest;
+
+    int num = 0;
+
+    if (scanf("%d", &num) > 0)
+    {
+        if (num > cur_largest)
+        {
+            second_largest = cur_largest;
+            cur_largest = num;
+        }
+        else if (num > second_largest)
+            second_largest = num;
+    }
+
+    return find_largest(--nums_left, cur_largest, second_largest);
+}
